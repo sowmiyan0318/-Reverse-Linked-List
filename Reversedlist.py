77c0b743d70970f3387e4336394cf497cc6386da
sowmiyan0318 <asowmiyan1@gmail.com>
2024-09-11 19:06:47 +0530

    Add files via upload

---
+++ Reversedlist.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def reverse_linked_list(head):
+    prev = None
+    current = head
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
+# Example usage:
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+
+reversed_head = reverse_linked_list(head)
+
+while reversed_head:
+    print(reversed_head.data, end=" -> ")
+    reversed_head = reversed_head.next
+print("None")
